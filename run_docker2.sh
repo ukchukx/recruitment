dae76f4ec0cfa94c526cd312b792f95610056762
Uk <ukchukundah@gmail.com>
2018-05-06 18:31:17 +0100

    Resolve naming conflicts

--- run_docker2.sh
+++ run_docker2.sh
@@ -3,33 +3,33 @@ docker run --restart=unless-stopped \
    -p 4000:4000 \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
-   --name recruitment \
+   --name recruitment0 \
    -d recruitment
 
 docker run --restart=unless-stopped \
    -p 4100:4000 \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
-   --name recruitment \
+   --name recruitment1 \
    -d recruitment
 
 docker run --restart=unless-stopped \
    -p 4200:4000 \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
-   --name recruitment \
+   --name recruitment2 \
    -d recruitment
 
 docker run --restart=unless-stopped \
    -p 4300:4000 \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
-   --name recruitment \
+   --name recruitment3 \
    -d recruitment
 
 docker run --restart=unless-stopped \
    -p 4400:4000 \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
-   --name recruitment \
+   --name recruitment4 \
    -d recruitment
