62fff867f853b4a467b85c5c8f1ecf64a1345305
Uk <ukchukundah@gmail.com>
2018-05-06 21:28:39 +0100

    Use host networking

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t recruitment -f Dockerfile .
+docker build -t recruitment .
--- run_docker2.sh
+++ run_docker2.sh
@@ -1,34 +1,39 @@
 #/bin/bash
 docker run --restart=unless-stopped \
-   -p 4000:4000 \
+   --network=host \
+   -e "PORT=4000" \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
    --name recruitment0 \
    -d recruitment
 
 docker run --restart=unless-stopped \
-   -p 4100:4000 \
+   --network=host \
+   -e "PORT=4100" \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
    --name recruitment1 \
    -d recruitment
 
 docker run --restart=unless-stopped \
-   -p 4200:4000 \
+   --network=host \
+   -e "PORT=4200" \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
    --name recruitment2 \
    -d recruitment
 
 docker run --restart=unless-stopped \
-   -p 4300:4000 \
+   --network=host \
+   -e "PORT=4300" \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
    --name recruitment3 \
    -d recruitment
 
 docker run --restart=unless-stopped \
-   -p 4400:4000 \
+   --network=host \
+   -e "PORT=4400" \
    -v=${PWD}/logs:/app/logs \
    -v=/var/www/html/nps/prison_cms_files:/var/www/html/nps/prison_cms_files \
    --name recruitment4 \
