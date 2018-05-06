92cb6b464835ecfd78ff0a2d2341f1d327357393
Uk <ukchukundah@gmail.com>
2018-05-06 18:12:48 +0100

    Replicate containers

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker build -t recruitment .
+docker build -t recruitment -f Dockerfile
+docker build -t recruitment -f Dockerfile1
+docker build -t recruitment -f Dockerfile2
+docker build -t recruitment -f Dockerfile3
