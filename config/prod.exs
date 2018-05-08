5f45be04c925caf64bca1d56868160c905b5885c
Uk <ukchukundah@gmail.com>
2018-05-08 05:57:28 +0100

    Allow info logs

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :recruitment, RecruitmentWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :warn
+config :logger, level: :info
 
 config :logger,
   backends: [{LoggerFileBackend, :info},
